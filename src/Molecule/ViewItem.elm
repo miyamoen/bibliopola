7220d718ae37e7438346cecff8af67339799d685
miyamoen <akihito.biblioteca@gmail.com>
2018-07-28 13:04:55 +0900

    add Tree line

--- src/Molecule/ViewItem.elm
+++ src/Molecule/ViewItem.elm
@@ -1,4 +1,4 @@
-module Molecule.ViewItem exposing (..)
+module Molecule.ViewItem exposing (view, viewItem)
 
 import Atom.Caret as Caret
 import Atom.File as File
@@ -18,34 +18,92 @@ import Types exposing (..)
 
 view : Zipper (View s v) -> MyElement s v
 view zipper =
-    let
-        { name, state } =
-            Zipper.current zipper
-
-        depth =
-            List.length <| Zipper.breadCrumbs zipper
-    in
-    el Box [ width fill ] <|
-        row None
-            [ spacing 10, moveRight <| toFloat depth * 20, verticalCenter ]
+    row None
+        []
+        [ spacer zipper
+        , row None
+            [ spacing 10, verticalCenter ]
             [ caret zipper
             , row None
                 [ spacing 5, verticalCenter ]
-                [ icon zipper, el Text [] <| text name ]
+                [ icon zipper, el Text [] <| text <| .name <| Zipper.current zipper ]
             ]
+        ]
+
+
+spacer : Zipper (View s v) -> Element (Styles s) vv msg
+spacer zipper =
+    let
+        depth =
+            List.length <| Zipper.breadCrumbs zipper
+    in
+    if depth == 0 then
+        empty
+    else
+        List.repeat (depth - 1) oneSpace
+            |> List.concat
+            |> flip List.append (lastSpace zipper)
+            |> row None []
+
+
+oneSpace : List (Element (Styles s) v msg)
+oneSpace =
+    [ el None [ width <| px 7 ] empty
+    , el None
+        [ width <| px 0
+        , inlineStyle [ "border-left" => borderCss ]
+        ]
+        empty
+    , el None [ width <| px 7 ] empty
+    ]
+
+
+lastSpace : Zipper (View s v) -> List (Element (Styles s) vv msg)
+lastSpace zipper =
+    if Zipper.isEmpty zipper then
+        [ el None [ width <| px 7 ] empty
+        , el None
+            [ width <| px 0
+            , inlineStyle [ "border-left" => borderCss ]
+            ]
+            empty
+        , el None
+            [ width <| px <| 7 + 16
+            , height <| px 12
+            , inlineStyle [ "border-bottom" => borderCss ]
+            ]
+            empty
+        , el None [ width <| px 10 ] empty
+        ]
+    else
+        [ el None [ width <| px 7 ] empty
+        , el None
+            [ width <| px 0
+            , inlineStyle [ "border-left" => borderCss ]
+            ]
+            empty
+        , el None
+            [ width <| px 5
+            , height <| px 12
+            , inlineStyle [ "border-bottom" => borderCss ]
+            ]
+            empty
+        , el None [ width <| px 2 ] empty
+        ]
+
+
+borderCss : String
+borderCss =
+    "2px solid rgba(138, 142, 180, 0.22)"
 
 
 caret : Zipper (View s v) -> MyElement s v
 caret zipper =
-    let
-        size =
-            15
-    in
     if Zipper.isEmpty zipper then
-        el None [ width <| px size, height <| px size ] empty
+        empty
     else
         Caret.view
-            { size = size
+            { size = 16
             , pallet = Grey
             , onClick = Just <| \_ -> SetViews <| Views.toggleTree zipper
             }
@@ -66,7 +124,7 @@ icon zipper =
         File.view
     )
     <|
-        { size = 15, pallet = Black, onClick = Nothing }
+        { size = 16, pallet = Black, onClick = Nothing }
 
 
 viewItem : View (Styles s) (Variation v)
