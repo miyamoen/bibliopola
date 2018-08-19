f475fdce9cc1768441b75141efed3c0b525eea9e
miyamoen <akihito.biblioteca@gmail.com>
2018-08-19 15:36:49 +0900

    Rename Types

--- src/Molecule/ShelfLine.elm
+++ src/Molecule/ShelfLine.elm
@@ -1,55 +1,53 @@
-module Molecule.ViewTreeLine exposing (view)
+module Molecule.ShelfLine exposing (view)
 
 import Atom.Caret as Caret
 import Atom.File as File
 import Atom.Folder as Folder
 import Color.Pallet exposing (Pallet(..))
-import Dict
 import Element exposing (..)
 import Element.Attributes exposing (..)
 import Element.Events exposing (onClick)
-import Lazy.Tree.Zipper as Zipper exposing (Zipper)
-import Model.ViewTree as ViewTree exposing (..)
+import Model.Shelf as Shelf exposing (..)
 import Types exposing (..)
 
 
-view : ViewTree s v -> MyElement s v
-view zipper =
+view : Shelf s v -> BibliopolaElement s v
+view shelf =
     row None
         []
-        [ spacer zipper
+        [ spacer shelf
         , row None
             [ spacing 10, verticalCenter ]
-            [ caret zipper
+            [ caret shelf
             , row None
-                (if Dict.isEmpty <| .variations <| Zipper.current zipper then
+                (if Shelf.hasNoStory shelf then
                     [ spacing 5, verticalCenter ]
                  else
-                    [ onClick <| GoToRoute <| getRoute zipper
+                    [ onClick <| GoToRoute <| Shelf.route shelf
                     , inlineStyle [ "cursor" => "pointer" ]
                     , spacing 5
                     , verticalCenter
                     ]
                 )
-                [ icon zipper
-                , el Text [] <| text <| .name <| Zipper.current zipper
+                [ icon shelf
+                , el Text [] <| text <| Shelf.name shelf
                 ]
             ]
         ]
 
 
-spacer : ViewTree s v -> Element (Styles s) vv msg
-spacer zipper =
+spacer : Shelf s v -> Element (Styles s) vv msg
+spacer shelf =
     let
         depth =
-            List.length <| Zipper.breadcrumbs zipper
+            Shelf.depth shelf
     in
     if depth == 0 then
         empty
     else
         List.repeat (depth - 1) oneSpace
             |> List.concat
-            |> flip List.append (lastSpace zipper)
+            |> flip List.append (lastSpace shelf)
             |> row None []
 
 
@@ -65,9 +63,9 @@ oneSpace =
     ]
 
 
-lastSpace : ViewTree s v -> List (Element (Styles s) vv msg)
-lastSpace zipper =
-    if Zipper.isEmpty zipper then
+lastSpace : Shelf s v -> List (Element (Styles s) vv msg)
+lastSpace shelf =
+    if not <| Shelf.hasBooks shelf then
         [ el None [ width <| px 7 ] empty
         , el None
             [ width <| px 0
@@ -104,18 +102,18 @@ borderCss =
     "2px solid rgba(138, 142, 180, 0.22)"
 
 
-caret : ViewTree s v -> MyElement s v
-caret zipper =
-    if Zipper.isEmpty zipper then
+caret : Shelf s v -> BibliopolaElement s v
+caret shelf =
+    if not <| Shelf.hasBooks shelf then
         empty
     else
         Caret.view
             { size = 16
             , pallet = Grey
-            , onClick = Just <| \_ -> SetViewTree <| ViewTree.toggleTree zipper
+            , onClick = Just <| \_ -> SetShelf <| Shelf.toggleShelf shelf
             }
         <|
-            case .state <| Zipper.current zipper of
+            case Shelf.state shelf of
                 Open ->
                     Caret.Down
 
@@ -123,9 +121,9 @@ caret zipper =
                     Caret.Right
 
 
-icon : ViewTree s v -> MyElement s v
-icon zipper =
-    (if Dict.isEmpty <| .variations <| Zipper.current zipper then
+icon : Shelf s v -> BibliopolaElement s v
+icon shelf =
+    (if Shelf.hasNoStory shelf then
         Folder.view
      else
         File.view
