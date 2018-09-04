75289a162cf141875ca960e1877dbea99fa47d1f
miyamoen <akihito.biblioteca@gmail.com>
2018-09-04 23:13:14 +0900

    Color.Pallet -> Color

---
+++ src/Color.elm
@@ -0,0 +1,23 @@
+module Color exposing (alphaGrey, black, blue, grey, white)
+
+import Element exposing (Color, rgb, rgba)
+
+
+black =
+    rgb 30 30 30
+
+
+white =
+    rgb 230 230 230
+
+
+blue =
+    rgb 45 129 204
+
+
+grey =
+    rgb 100 100 100
+
+
+alphaGrey =
+    rgba 138 142 180 0.22
