ab79a71f8ba8f10c6f7a23fdafb470a5e17f9cf0
miyamoen <akihito.biblioteca@gmail.com>
2018-09-08 22:33:47 +0900

    ShelfItem for 0.19

---
+++ src/Molecule/ShelfItem.elm
@@ -0,0 +1,77 @@
+module Molecule.ShelfItem exposing (view)
+
+import Atom.Constant exposing (fontSize, iconSize, space)
+import Atom.Icon.Book as Book
+import Atom.Icon.Books as Books
+import Atom.Icon.Caret as Caret
+import Atom.Icon.RuledLine as RuledLine
+import Color
+import Element exposing (..)
+import Element.Events exposing (onClick)
+import Element.Font as Font
+import Model.Book as Book
+import Model.Shelf as Shelf
+import Types exposing (Msg(..), Shelf)
+
+
+view : Shelf -> Element Msg
+view shelf =
+    row
+        [ spacing <| space 1
+        , onClick <| SetShelf <| Shelf.updateBook Book.toggleShelf shelf
+        ]
+        [ row [] <| ruledLine shelf
+        , icon shelf
+        , el [ Font.size <| fontSize 2 ] <|
+            text <|
+                Shelf.mapCurrentBook Book.title shelf
+        ]
+
+
+ruledLine : Shelf -> List (Element msg)
+ruledLine shelf =
+    let
+        depth =
+            Shelf.depth shelf
+    in
+    case depth of
+        0 ->
+            [ caret shelf ]
+
+        1 ->
+            [ RuledLine.view { size = 1 } RuledLine.VerticalRight
+            , caret shelf
+            ]
+
+        _ ->
+            List.repeat (depth - 1)
+                (RuledLine.view { size = 1 } RuledLine.Vertical)
+                ++ [ RuledLine.view { size = 1 } RuledLine.VerticalRight
+                   , caret shelf
+                   ]
+
+
+caret : Shelf -> Element msg
+caret shelf =
+    if not <| Shelf.hasShelves shelf then
+        RuledLine.view { size = 1 } RuledLine.Horizontal
+
+    else
+        Caret.view { size = 1, color = Color.grey, onClick = Nothing } <|
+            if Shelf.mapCurrentBook Book.shelfIsOpen shelf then
+                Caret.Down
+
+            else
+                Caret.Right
+
+
+icon : Shelf -> Element msg
+icon shelf =
+    (if Shelf.hasShelves shelf then
+        Books.view
+
+     else
+        Book.view
+    )
+    <|
+        { size = iconSize 1, color = Color.black, onClick = Nothing }
