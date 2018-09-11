1695a5cc2a8c6a11b2bf73027e1a0326df3f84e7
miyamoen <akihito.biblioteca@gmail.com>
2018-09-12 06:58:39 +0900

    Adjust sizes, but give up

--- src/Page/Main.elm
+++ src/Page/Main.elm
@@ -12,7 +12,9 @@ import Types exposing (..)
 view : SubModel a -> Element Msg
 view model =
     column
-        [ padding <| space 1
+        [ width fill
+        , height fill
+        , padding <| space 1
         , spacing <| space 1
         , Font.family
             [ Font.external
@@ -21,11 +23,12 @@ view model =
                 }
             ]
         ]
-        [ row [ height <| px 400 ]
+        [ row [ height fill ]
             [ el [ scrollbars, width <| px 200, height fill ] <|
                 ShelfTree.view model.shelf
-            , BookPage.view model.shelf
+            , el [ scrollbars, width fill, height fill ] <|
+                BookPage.view model.shelf
             ]
-        , el [ scrollbarY, clipX, height <| px 210 ] <|
+        , el [ scrollbarY, clipX, height <| px 210, width fill ] <|
             Panel.view model
         ]
