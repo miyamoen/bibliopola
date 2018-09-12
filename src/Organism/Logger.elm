f47bbb09d17295a4de54cea40f96c18bde011229
miyamoen <akihito.biblioteca@gmail.com>
2018-09-13 07:07:11 +0900

    Align Ban button to top in Logger panel

--- src/Organism/Logger.elm
+++ src/Organism/Logger.elm
@@ -13,11 +13,12 @@ view : List Log -> Element Msg
 view logs =
     row
         [ spacing <| space 1 ]
-        [ Ban.view
-            { onClick = Just ClearLogs
-            , color = Color.black
-            , size = 2
-            }
+        [ el [ alignTop ] <|
+            Ban.view
+                { onClick = Just ClearLogs
+                , color = Color.black
+                , size = 2
+                }
         , Keyed.column [ spacing <| space 1, width fill ] <|
             List.map (\log -> ( String.fromInt log.id, Log.view log )) logs
         ]
